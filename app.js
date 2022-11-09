0fdcf74ca77232f29bf509361b7ddc0ed24de1ea
Anthony <anthony.rabot@gmail.com>
2022-11-09 17:53:12 +0100

    Add MongoDB connexion. Mongo is managed over Docker with port forwarding

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
 const express = require('express')
+const mongoose = require('mongoose')
+
+mongoose.connect('mongodb://127.0.0.1:49154', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+    .then(() => console.log('Connexion à MongoDB réussie !'))
+    .catch(() => console.log('Connexion à MongoDB échouée !'));
+
 const app = express();
 
 app.use((req, res) => {
-    res.json({ message: 'Votre requête a bien été très bien reçue !' });
+    res.json({message: 'Votre requête a bien été très bien reçue !'});
 })
 
 module.exports = app;
