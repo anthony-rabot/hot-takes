4160d09922b9ba03cea10c8236c3abe288ae2ccd
Anthony <anthony.rabot@gmail.com>
2022-11-10 14:40:55 +0100

    Add routes for user and CORS headers

--- app.js
+++ app.js
@@ -1,18 +1,31 @@
 require('dotenv').config()
 const mongoose = require('mongoose')
 const express = require('express')
+const userRoutes = require('./routes/user')
 
+const app = express()
+
+// MongoDB connexion
 mongoose.connect(process.env.MONGODB_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true
-})
+    })
     .then(() => console.log('Connexion à MongoDB réussie !'))
-    .catch(() => console.log('Connexion à MongoDB échouée !'));
+    .catch(() => console.log('Connexion à MongoDB échouée !'))
 
-const app = express();
 
-app.use((req, res) => {
-    res.json({message: 'Votre requête a bien été très bien reçue !'});
+// CORS headers
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*')
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization')
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS')
+    next()
 })
 
-module.exports = app;
+// Express Middleware to parse JSON request
+app.use(express.json())
+
+// Routes
+app.use('/api/auth', userRoutes)
+
+module.exports = app
