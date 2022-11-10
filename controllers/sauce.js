7c06300382745d5fd7721a04cb854390311289d7
Anthony <anthony.rabot@gmail.com>
2022-11-10 20:34:28 +0100

    Add functions to get all sauces, update and delete one

--- controllers/sauce.js
+++ controllers/sauce.js
@@ -15,4 +15,44 @@ exports.createSauce = (req, res, next) => {
     sauce.save()
         .then(() => { res.status(201).json({message: 'Sauce enregistrée !'})})
         .catch(error => { res.status(400).json( { error })})
-};
+}
+
+exports.getSauces = (req, res, next) => {
+    Sauce.find()
+        .then(sauces => res.status(200).json(sauces))
+        .catch(error => res.status(400).json({ error }))
+}
+
+exports.getSauce = (req, res, next) => {
+    Sauce.findOne({ _id: req.params.id })
+        .then(sauce => res.status(200).json(sauce))
+        .catch(error => res.status(404).json({ error }));
+}
+
+exports.modifySauce = (req, res, next) => {
+    const sauceObject = req.file ? {
+        ...JSON.parse(req.body.sauce),
+        imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
+    } : { ...req.body }
+
+    delete sauceObject._userId
+    Sauce.findOne({_id: req.params.id})
+        .then((sauce) => {
+            if (sauce.userId != req.auth.userId) {
+                res.status(401).json({ message : "Vous n'êtes pas autorisé à modifier cette sauce"})
+            } else {
+                Sauce.updateOne({ _id: req.params.id}, { ...sauceObject, _id: req.params.id})
+                    .then(() => res.status(200).json({message : 'Sauce modifiée'}))
+                    .catch(error => res.status(401).json({ error }))
+            }
+        })
+        .catch((error) => {
+            res.status(400).json({ error })
+        })
+}
+
+exports.deleteSauce = (req, res, next) => {
+    Sauce.deleteOne({ _id: req.params.id })
+        .then(sauce => res.status(200).json({message: 'Sauce supprimée'}))
+        .catch(error => res.status(400).json({ error }));
+}
