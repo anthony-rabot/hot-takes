065cb34f0425f17749453174e4a7d9d48ae9082a
Anthony <anthony.rabot@gmail.com>
2022-11-10 20:35:12 +0100

    Add routes

--- routes/sauce.js
+++ routes/sauce.js
@@ -8,5 +8,9 @@ const sauceController = require('../controllers/sauce')
 
 // Define routes and Controller to use
 router.post('/sauces', auth, multer, sauceController.createSauce)
+router.get('/sauces', auth, sauceController.getSauces)
+router.get('/sauces/:id', auth, sauceController.getSauce)
+router.put('/sauces/:id', auth, multer, sauceController.modifySauce)
+router.delete('/sauces/:id', auth, sauceController.deleteSauce)
 
 module.exports = router
