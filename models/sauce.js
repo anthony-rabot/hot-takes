ad525915a6965238fa93241772f0fe92551b0e62
Anthony <anthony.rabot@gmail.com>
2022-11-10 20:34:59 +0100

    Add userId, delete and modifiy were not visibles

--- models/sauce.js
+++ models/sauce.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
 const sauceSchema = new Schema({
+    userId: { type: String, required: true },
     name: { type: String, required: true },
     manufacturer: { type: String, required: true },
     description: { type: String, required: true },
