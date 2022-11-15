97ca8422044f666d69a677ee1df8eabb00de76c3
anthony <anthony.rabot@gmail.com>
2022-11-15 15:29:33 +0100

    Add user creation validation

--- routes/user.js
+++ routes/user.js
@@ -2,9 +2,10 @@ const express = require('express')
 const router = express.Router()
 
 const userController = require('../controllers/user')
+const { userValidationRules, validate } = require('../middleware/user-validator.js')
 
 // Define routes and Controller to use
-router.post('/signup', userController.createUser)
+router.post('/signup', userValidationRules(), validate, userController.createUser)
 router.post('/login', userController.loginUser)
 
 module.exports = router
