8cee58b24fc98ae9f1de10c6d1200b9780cefc82
anthony <anthony.rabot@gmail.com>
2022-11-15 11:50:10 +0100

    FIX CORS / image names

--- app.js
+++ app.js
@@ -3,17 +3,20 @@ const mongoose = require('mongoose')
 const express = require('express')
 const path = require('path')
 
-// Security
+// Security packages
+const cors = require('cors')
 const helmet = require('helmet')
 const rateLimit = require('express-rate-limit')
 const mongoSanitize = require("express-mongo-sanitize")
+const hpp = require('hpp')
 
-const apiLimiter = rateLimit({
-    windowMs: 15 * 60 * 1000, // 15 minutes
-    max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
-    standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
-    legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+// MongoDB connexion
+mongoose.connect(process.env.MONGODB_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
 })
+    .then(() => console.log('Connexion à MongoDB réussie !'))
+    .catch(() => console.log('Connexion à MongoDB échouée !'))
 
 // Routes
 const userRoutes = require('./routes/user')
@@ -21,35 +24,41 @@ const sauceRoutes = require('./routes/sauce')
 
 const app = express()
 
-// MongoDB connexion
-mongoose.connect(process.env.MONGODB_URL, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-    })
-    .then(() => console.log('Connexion à MongoDB réussie !'))
-    .catch(() => console.log('Connexion à MongoDB échouée !'))
-
-
-// CORS headers
-app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*')
-    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization')
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS')
-    next()
-})
-
 // Express Middleware to parse JSON request
 app.use(express.json())
 
-// Headers security
-app.use(helmet())
+// Security
+
+// Helmet add some security defaults headers https://stackoverflow.com/questions/70752770/helmet-express-err-blocked-by-response-notsameorigin-200
+app.use(
+    helmet({
+        crossOriginResourcePolicy: false
+    })
+)
 app.disable('x-powered-by')
 
 // Limit number of request for same IP
+const apiLimiter = rateLimit({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+    standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+    legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+})
+
 app.use(apiLimiter)
 
 // Prevent MongoDB from injection & Cross-Site scripting https://javascript.plainenglish.io/how-to-sanitize-your-express-app-against-mongodb-injection-cross-site-scripting-6a22f4e822aa
 app.use(mongoSanitize())
+
+// HPP protect against HTTP Parameter Pollution attacks
+app.use(hpp())
+
+// CORS
+app.use(cors({
+    "origin": "*",
+    "allowedHeaders": "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization",
+    "methods": "GET, PUT, POST, DELETE"
+}));
 
 // Routes
 app.use('/images', express.static(path.join(__dirname, 'images')))
--- middleware/multer-config.js
+++ middleware/multer-config.js
@@ -1,20 +1,23 @@
 const multer = require('multer')
 
 const MIME_TYPES = {
-    'image/jpg': 'jpg',
-    'image/jpeg': 'jpg',
-    'image/png': 'png',
-    'image/webp': 'webp'
+    "image/jpg": "jpg",
+    "image/jpeg": "jpg",
+    "image/png": "png",
+    "image/webp": "webp"
 }
 
+// https://www.npmjs.com/package/multer doc with values of file
+
 const storage = multer.diskStorage({
-    destination: (req, file, callback) => {
-        callback(null, 'images')
+    destination: (req, file, cb) => {
+        cb(null, 'images')
     },
     filename: (req, file, callback) => {
-        const name = file.originalname.split(' ').join('_')
+        const cleanName = file.originalname.split(' ').join('_')
+        const nameWithoutExtension = cleanName.split('.')
         const extension = MIME_TYPES[file.mimetype]
-        callback(null, name + Date.now() + '.' + extension)
+        callback(null, nameWithoutExtension[0] + Date.now() + '.' + extension)
     }
 })
 
