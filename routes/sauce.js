34a640e20ed4c3e2b5c781fa7299c121979e5482
Anthony <anthony.rabot@gmail.com>
2022-11-17 15:11:45 +0100

    Add validation rules on modify sauce route

--- routes/sauce.js
+++ routes/sauce.js
@@ -7,14 +7,11 @@ const sauceValidationRules = require("../middleware/sauce-validator")
 
 const sauceController = require('../controllers/sauce')
 
-
-
-
 // Define routes and Controller to use
 router.post('/sauces', auth, multer, sauceValidationRules, sauceController.createSauce)
 router.get('/sauces', auth, sauceController.getSauces)
 router.get('/sauces/:id', auth, sauceController.getSauce)
-router.put('/sauces/:id', auth, multer, sauceController.modifySauce)
+router.put('/sauces/:id', auth, multer, sauceValidationRules, sauceController.modifySauce)
 router.delete('/sauces/:id', auth, sauceController.deleteSauce)
 router.post('/sauces/:id/like', auth, sauceController.likeSauce)
 
